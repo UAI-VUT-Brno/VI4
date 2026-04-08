c0244b948bebe969c5e8cf8248f992d844baba77
JanFaltynekVUT <qmfaltynek@vutbr.cz>
2026-04-08 11:44:40 +0200

    Delete mqtt python directory

--- mqtt python/task03.py
+++
@@ -1,8 +0,0 @@
-# TODO: Zrecyklujte kód z minulé úlohy a přihlašte se 
-# k odběru všeho v topicu test/
-
-# TODO: Následně zkoušejte jak funguje nahrazování částí 
-# názvu topicu pomocí # a +
-
-# TODO: Nakonec se přihlašte k odběru všeho v topicu test,
-# co má třetí úroveň nazvanou "hello"
